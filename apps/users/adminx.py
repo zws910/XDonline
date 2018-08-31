33cf4363027f386841673edd381031d24ff183fa
zws910 <vincentzheng0910@gmail.com>
2018-08-31 22:16:00 +0800

    admin register - users

---
+++ apps/users/adminx.py
@@ -0,0 +1,24 @@
+import xadmin
+
+from .models import EmailVerifyRecord, Banner
+
+
+class EmailVerifyRecordAdmin(object):
+    # 显示的列
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索的字段, 不要添加时间搜索
+    search_fields = ['code', 'email', 'send_type']
+    # 过滤
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(Banner, BannerAdmin)

1ca79aa0a7b4840379a2af9cc97b3b143df2598b
zws910 <vincentzheng0910@gmail.com>
2018-08-31 20:57:23 +0800

    config timezone

--- XDonline/settings.py
+++ XDonline/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'course',
     'operation',
     'organization',
+    'xadmin',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Asia/Shanghai'
 
 TIME_ZONE = 'UTC'
 
@@ -125,7 +127,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/

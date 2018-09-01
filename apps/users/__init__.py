52dd7a59869a22facf156b367e77d6e011be7f13
zws910 <vincentzheng0910@gmail.com>
2018-09-01 16:43:52 +0800

    admin - rename apps

--- apps/users/__init__.py
+++ apps/users/__init__.py
@@ -1,4 +1,3 @@
-import pymysql
 
-pymysql.install_as_MySQLdb()
+default_app_config = 'users.apps.UsersConfig'
 
--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,8 +1,35 @@
 import xadmin
+from xadmin import views
 
 from .models import EmailVerifyRecord, Banner
 
 
+# 配置xadmin全站的主题
+class BaseSetting(object):
+    # 开启主题功能
+    enable_themes = True
+    use_bootswatch = True
+
+
+# 将基本配置管理与view绑定
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+
+# 全局修改
+class GlobalSettings(object):
+    # 修改title
+    site_title = '新道后台管理'
+    # 修改footer
+    site_footer = '新道文化教育公司'
+    # 收起菜单
+    menu_style = 'accordion'
+
+
+# 将footer和title信息注册
+xadmin.site.register(views.CommAdminView, GlobalSettings)
+
+
+# 注册models
 class EmailVerifyRecordAdmin(object):
     # 显示的列
     list_display = ['code', 'email', 'send_type', 'send_time']

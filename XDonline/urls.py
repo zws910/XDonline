99bc745d93ecb4d5e18327d56583a1ed53d96c39
zws910 <vincentzheng0910@gmail.com>
2018-10-13 19:37:34 +0800

    static files - index login

--- XDonline/urls.py
+++ XDonline/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 
 from django.views.generic import TemplateView
 
+from users import views
+
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.user_login, name='login'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
+
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        # 获取用户提交的用户名和密码
+        user_name = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        # 成功返回user对象, 失败None
+        user = authenticate(username=user_name, password=password)
+        # 如果不是null说明验证成功
+        if user is not None:
+            # 登录
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html', {'msg': '用户名或密码错误'})
+
+    elif request.method == 'GET':
+        return render(request, 'login.html')

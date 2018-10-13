491004d0c79f2cc0614d999711ffe9a56a833fb3
zws910 <vincentzheng0910@gmail.com>
2018-10-13 21:02:59 +0800

    add - login authentication

--- XDonline/urls.py
+++ XDonline/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 
 from django.views.generic import TemplateView
 
-from users import views
+from users.views import LoginView
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('login/', views.user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic.base import View
+from .forms import LoginForm
 
 from users.models import UserProfile
 
@@ -23,26 +25,48 @@ class CustomBackend(ModelBackend):
             return None
 
 
-
-
-
-
-
-def user_login(request):
-    """用户名密码登录"""
-    if request.method == "POST":
-        # 获取用户提交的用户名和密码
-        user_name = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        # 成功返回user对象, 失败None
-        user = authenticate(username=user_name, password=password)
-        # 如果不是null说明验证成功
-        if user is not None:
-            # 登录
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {'msg': '用户名或密码错误'})
-
-    elif request.method == 'GET':
+class LoginView(View):
+    def get(self, request):
         return render(request, 'login.html')
+
+    def post(self, request):
+        # 实例化表单验证
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            # 获取用户提交的用户名和密码
+            user_name = request.POST.get('username', None)
+            pass_word = request.POST.get('password', None)
+            # 成功返回user对象, 失败None
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                # 验证成功
+                # 登录
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                # 验证失败
+                return render(request, 'login.html', {'msg': '用户名或密码错误', 'login_form': login_form})
+        else:
+            return render(request, 'login.html', {'login_form': login_form})
+
+
+
+# def user_login(request):
+#     """用户名密码登录"""
+#     if request.method == "POST":
+#         # 获取用户提交的用户名和密码
+#         user_name = request.POST.get('username', None)
+#         password = request.POST.get('password', None)
+#         # 成功返回user对象, 失败None
+#         user = authenticate(username=user_name, password=password)
+#         # 如果不是null说明验证成功
+#         if user is not None:
+#             # 登录
+#             login(request, user)
+#             return render(request, 'index.html')
+#         else:
+#             return render(request, 'login.html', {'msg': '用户名或密码错误'})
+#
+#     elif request.method == 'GET':
+#         return render(request, 'login.html')
